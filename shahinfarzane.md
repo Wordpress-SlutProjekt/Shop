4b324d00d1065ef6b206a033f02a3840d653c3b6
Shahin Farzane <shahinfarzane90@hotmail.com>
2021-05-03 14:38:31 +0200

    Single blog post template has been added

---
+++ shahinfarzane.md
@@ -0,0 +1,20 @@
+
+
+Day1
+Set up the Github
+Set up Online Mysql Database
+Installed Wordpress
+Made User accounts for everybody
+Posted 5 blog posts
+Added a Few products
+Added product attributes
+Set up WooCommerce
+
+
+Day2
+
+Made the Contactform template file
+Made the contact form fields with ACF
+
+Day 3
+Made the Single.php blog post page
--- wp-config.php
+++ wp-config.php
@@ -23,13 +23,13 @@
 define( 'DB_NAME', 'wp_projekt_grupp1' );
 
 /** MySQL database username */
-define( 'DB_USER', 'shahinfarzane' );
+define( 'DB_USER', 'root' );
 
 /** MySQL database password */
-define( 'DB_PASSWORD', 'shahinfarzane1' );
+define( 'DB_PASSWORD', 'root' );
 
 /** MySQL hostname */
-define( 'DB_HOST', 'mysql.shahinfarzane.com' );
+define( 'DB_HOST', 'localhost' );
 
 /** Database Charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8' );
