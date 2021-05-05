2039e7325200528018f3b1bf5be6d1b592f2f02a
Shahin-Sayadi <Shahin_sayadi@hotmail.com>
2021-05-05 11:21:31 +0200

    syntax error

--- wp-content/themes/slutprojekt/functions.php
+++ wp-content/themes/slutprojekt/functions.php
@@ -10,7 +10,7 @@ add_theme_support('woocommerce');
 
 function register_menus() {
     register_nav_menu('header-menu', 'header-meny');
-    
+    register_nav_menu( 'category-menu' , 'Kategorimeny' );
     
     }
 
@@ -182,9 +182,7 @@ register_sidebar(
 
 /* header menus */
 
-function register_menus() {
-    register_nav_menu('header-menu', 'header-meny');
-    register_nav_menu( 'category-menu' , 'Kategorimeny' );
+
     
     function wpdocs_filter_wp_title( $title, $sep ) {
         global $paged, $page;
