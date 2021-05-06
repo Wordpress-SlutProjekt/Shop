a9ca15460dddefffc7a81d2f8ad246eaad63585e
Fredrik Ljungqvist <fljungqvist@gmail.com>
2021-05-06 09:16:26 +0200

    cart work

--- wp-content/themes/slutprojekt/page-cart.php
+++ wp-content/themes/slutprojekt/page-cart.php
@@ -1,22 +1,45 @@
 <?php get_header();?>
 
-<section>
+<section class="section-cart">
     <div class="container">
         <div class="row">
-            <div class="col-xs-12 col-md-8 col-md-offset-2">
+    <div class="section-title">
+                    <h4>Cart</h4>
+                </div>
+
+                <section class="shop-cart spad">
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <div class="shop__cart__table">
+                        <table>
+                            <thead>
+                                <tr>
+                                    <th>Product</th>
+                                    <th>Price</th>
+                                    <th>Quantity</th>
+                                    <th>Total</th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+            <!-- <div class="col-xs-12 col-md-8 col-md-offset-2"> -->
                 <?php 
                     while(have_posts()) {
                         the_post();
                 ?>
                     <br><br>
-                <h1><?php the_title();?></h1>
+                <h1><?php //the_title();?></h1>
                 
                 <?php the_content();?>
                 <?php
                   } 
                 ?>
-       
-            </div>
+            
+            <!-- </div> -->
+                            </tbody>
+                        </table>
+        </div>
         </div>
 </div>
 </section>
