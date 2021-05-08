26d5f05868e74e5fb5adf5198db38d6bbb30620f
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-05-08 11:49:08 +0200

    New changes

--- wp-content/themes/slutprojekt/home.php
+++ wp-content/themes/slutprojekt/home.php
@@ -1,40 +1,33 @@
-<?php get_header(); ?>  
-
-
-
-
-
-
+<?php get_header(); ?>
 
 <section class="blog spad">
-        <div class="container">
-            <div class="row">
-            <?php
-  while(have_posts()){
-    the_post();
-    ?>
-                <div class="col-lg-4 col-md-4 col-sm-6">
-                    <div class="blog__item">
-                        <div class="blog__item__pic large__item set-bg" data-setbg="<?= get_the_post_thumbnail_url(); ?>"></div>
-                        <div class="blog__item__text">
-                            <h6><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h6>
-                            <ul>
-                                <li>by <span><a href="<?php the_permalink() ?>"><?php the_author(); ?></a></span></li>
-                                <li><?php the_time(); ?></li>
-                            </ul>
-                        </div>
-                    </div>
-  
-
-                </div>
-                <?php } ?>
-              
-    
+  <div class="container">
+    <div class="row">
+      <?php
+      while (have_posts()) {
+        the_post();
+      ?>
+        <div class="col-lg-4 col-md-4 col-sm-6">
+          <div class="blog__item">
+            <div class="blog__item__pic large__item set-bg" data-setbg="<?= get_the_post_thumbnail_url(); ?>"></div>
+            <div class="blog__item__text">
+              <h6><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h6>
+              <ul>
+                <li>by <span><a href="<?php the_permalink() ?>"><?php the_author(); ?></a></span></li>
+                <li><?php the_time(); ?></li>
+              </ul>
             </div>
+          </div>
+
         </div>
-    </section>
+      <?php } ?>
+
+
+    </div>
+  </div>
+</section>
 
 
 
 
-<?php get_footer(); ?>  
+<?php get_footer(); ?>
