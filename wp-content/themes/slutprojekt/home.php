df6648965e7b31e84d1620471fc3997313c41a05
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-05-08 11:46:27 +0200

    Blog page - Home page added

--- wp-content/themes/slutprojekt/home.php
+++ wp-content/themes/slutprojekt/home.php
@@ -2,28 +2,37 @@
 
 
 
- 
-<?php
+
+
+
+
+<section class="blog spad">
+        <div class="container">
+            <div class="row">
+            <?php
   while(have_posts()){
     the_post();
     ?>
+                <div class="col-lg-4 col-md-4 col-sm-6">
+                    <div class="blog__item">
+                        <div class="blog__item__pic large__item set-bg" data-setbg="<?= get_the_post_thumbnail_url(); ?>"></div>
+                        <div class="blog__item__text">
+                            <h6><a href="<?php the_permalink();?>"><?php the_title(); ?></a></h6>
+                            <ul>
+                                <li>by <span><a href="<?php the_permalink() ?>"><?php the_author(); ?></a></span></li>
+                                <li><?php the_time(); ?></li>
+                            </ul>
+                        </div>
+                    </div>
+  
 
-<article id="book" onClick="location.href='<?php the_permalink();?>'" class="card">
-  <div class="photoDiv">
-    <img class="postThumbnail" src="<?= get_the_post_thumbnail_url(); ?>" >
-  </div>
-  <div class="desc">
-      <?= get_the_date('Y-d-m'); ?>
-      <div class="newsTitle">
-        <h1><?php the_title(); ?></h1>
-      </div>
-      <p><?php the_excerpt(); ?></p>
-    </div>
-    </article>
-
-    <?php } ?>
-</main>
-
+                </div>
+                <?php } ?>
+              
+    
+            </div>
+        </div>
+    </section>
 
 
 
--- wp-content/themes/slutprojekt/single.php
+++ wp-content/themes/slutprojekt/single.php
@@ -1,45 +1,55 @@
-<?php 
-get_header();
-?>
-<section class="product-details spad">
+<?php get_header(); ?>
+
+
+<section class="blog-details spad">
         <div class="container">
             <div class="row">
-            <?php if (have_posts()) : ?>
-        <?php if (($wp_query->post_count) > 1) : ?>
-        <?php while (have_posts()) : the_post(); ?>
-       <?php endwhile; ?>
-       <?php else : ?>
-
-     <?php while (have_posts()) : the_post(); ?>
-                <div class="col-lg-6">
-                    <div class="product__details__pic">
-                        <div class="product__details__pic__left product__thumb nice-scroll">
-                        <?php the_post_thumbnail();?> 
+            <?php
+  while(have_posts()){
+    the_post();
+    ?>
+                <div class="col-lg-8 col-md-8">
+                    <div class="blog__details__content">
+                        <div class="blog__details__item">
+                            <img src=" <?php the_post_thumbnail();?>" alt="">
+                            <div class="blog__details__item__title">
+                                <h4><?php the_title(); ?></h4>
+                                <ul>
+                                    <li>by <span><a href="<?php the_permalink() ?>"><?php the_author();?></a></span></li>
+                                    <li><?php the_time(); ?></li>
+                                    <li><?php echo sprintf(__('<a href="%s">%s comment(s)</a>', 'textdomain'), get_comments_link(),get_comments_number()); ?></li>
+                                </ul>
+                            </div>
                         </div>
-
-                    </div>
-                </div>
-                <div class="col-lg-6">
-                    <div class="product__details__text">
-                        <h3><?php the_title(); ?></h3>
-     
-                      
-
-                        <?php the_content(); ?>
-
+                        <div class="blog__details__desc">
+                        <p><?php the_content() ?></p>
                         </div>
-                  
-                    </div>
-                </div>
-          
-            </div>
+    
+    
+                        <div class="blog__details__btns">
+                            <div class="row">
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="blog__details__btn__item">
+                                        <h6> <?php previous_post_link()?>        	  
+<i class="fa fa-angle-left"></i> </h6>
+                                    </div>
+                                </div>
          
+                                <div class="col-lg-6 col-md-6 col-sm-6">
+                                    <div class="blog__details__btn__item blog__details__btn__item--next">
+                                        <h6><?php next_post_link() ?> <i class="fa fa-angle-right"></i></h6>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <?php } ?>
+
+            </div>
         </div>
     </section>
-    <?php endwhile; ?>
 
-<?php endif; ?>
 
-<?php else : ?>
-
-<?php endif; ?>
+    <?php get_footer(); ?>
