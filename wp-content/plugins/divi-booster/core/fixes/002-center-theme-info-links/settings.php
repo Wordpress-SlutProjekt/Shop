fac52898736f2889e12e34df0fcce1faedc16d68
Shahin-Sayadi <Shahin_sayadi@hotmail.com>
2021-05-10 14:24:46 +0200

    revert

--- wp-content/plugins/divi-booster/core/fixes/002-center-theme-info-links/settings.php
+++
@@ -1,9 +0,0 @@
-<?php 
-if (!defined('ABSPATH')) { exit(); } // No direct access
-
-function db002_add_setting($plugin) { 
-	$plugin->setting_start(); 
-	$plugin->checkbox(__FILE__); ?> Center the footer credits<?php
-	$plugin->setting_end(); 
-} 
-$wtfdivi->add_setting('footer-bottombar', 'db002_add_setting');	
