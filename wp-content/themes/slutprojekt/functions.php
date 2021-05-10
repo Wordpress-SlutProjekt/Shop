98fe09cabb5dcaaaf13aad55587ee814bebe8bc0
Shahfarzane <42602105+Shahfarzane@users.noreply.github.com>
2021-05-10 15:34:07 +0200

    maybe fixed

--- wp-content/themes/slutprojekt/functions.php
+++ wp-content/themes/slutprojekt/functions.php
@@ -163,15 +163,6 @@ register_sidebar(
 
 
 
-
-
-    // function my_acf_google_map_api( $api ){
-    //     $api['AIzaSyBh3p65wVsOQhCGlnsO8uXpghP9aSy0KV4'] = 'xxx';
-    //     return $api;
-    // }
-    // add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
-
-
     function fix_gmaps_api_key() {
         if(mb_strlen(acf_get_setting("google_api_key")) <= 0){
             acf_update_setting("google_api_key", "AIzaSyBh3p65wVsOQhCGlnsO8uXpghP9aSy0KV4");
